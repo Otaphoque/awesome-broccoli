b5f9b43a1994c014e434563ea3dce04b3fb19a87
Constance <79022298+knstnce@users.noreply.github.com>
2022-09-16 23:32:05 -0400

    moved the Manifest to the resource folder, now i don't have the missing manifest file error. yay. small improvements. now i gotta figure out the missing javafx componenents EVEN THOUGH THEY ARE ADDED. god i love programing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens truc.Controller to javafx.controls, javafx.fxml;
     exports truc;
