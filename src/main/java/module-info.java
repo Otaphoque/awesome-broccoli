2780a21078e0719fcfaec63ecefd087323aea9f6
Constance <79022298+knstnce@users.noreply.github.com>
2022-10-17 10:35:56 -0400

    Updated the buttons (they use css code now they're not images)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.base;
 
-    opens truc.Controller to javafx.controls, javafx.fxml;
+    opens truc.Controller to javafx.controls, javafx.fxml, javafx.graphics;
     exports truc;
 }
